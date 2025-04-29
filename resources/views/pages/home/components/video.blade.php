c95a138ec59441fefbcc58f1fb04b1f498938b2f
Kraprow <kamalat.g@nsru.ac.th>
2025-04-29 15:02:17 +0700

    ีupdate

--- resources/views/pages/home/components/video.blade.php
+++ resources/views/pages/home/components/video.blade.php
@@ -8,7 +8,7 @@
 
 <main class="d-flex justify-content-center align-items-center">
     <video autoplay muted loop playsinline>
-        <source src="{{ asset('images/video/อบตเสม็ดใต้.webm') }}" type="video/mp4">
+        <source src="{{ asset('images/video/งาน_VDOเสม็ด_2.mp4') }}" type="video/mp4">
         <!-- ตัวเลือกสำรองสำหรับเบราว์เซอร์ที่ไม่รองรับ -->
         Your browser does not support the video tag.
     </video>
